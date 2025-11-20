7129014baeee9fb967e119b74658ac3cc5c14d92
Norman Sáenz A <saenznorman73@gmail.com>
2025-11-20 17:33:58 -0600

    Prueba

--- Assets/Lobby/IGameStarter.cs
+++ Assets/Lobby/IGameStarter.cs
@@ -3,4 +3,5 @@ public interface IGameStarter
 {
     void StartHost();                // Host crea partida
     void StartClient(string token);  // Cliente se une (IP si LAN, joinCode si Relay)
+    
 }
